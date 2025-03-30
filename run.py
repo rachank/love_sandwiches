6d13b8fe5a2567ca81e508258c1e35130c688f33
rachank <ra24hank@proton.com>
2025-03-30 21:06:17 +0200

    Add last section of code and corections.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import json
 
 
 SCOPE = [
@@ -8,6 +9,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+creds = json.load(open('creds.json'))
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
@@ -153,9 +155,9 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
-
-print("Welcome to Love Sandwiches Data Automation")
+print("Welcome to Love Sandwiches Data Automation.\n")
 stock_data = main()
 
 # Write you code below this comment
